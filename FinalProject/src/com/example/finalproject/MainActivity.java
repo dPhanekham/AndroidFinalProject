bc8c9b3d7cf308d970cda43c533cce2191dfcfaf
Derek Phanekham <dphanekham@smu.edu>
2013-04-30 21:00:32 -0500

    google map setup. with errors

--- FinalProject/src/com/example/finalproject/MainActivity.java
+++ FinalProject/src/com/example/finalproject/MainActivity.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends Activity {
+	
+	  private GoogleMap map;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	        .getMap();
+	    
+	    
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
+	  }
 }
