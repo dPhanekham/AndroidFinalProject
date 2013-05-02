7adc8a44c7c2db278c8923ac85cfe77d9556bf75
Derek Phanekham <dphanekham@smu.edu>
2013-05-02 00:34:56 -0500

    update to working packages and whatnot

--- FinalProject/src/com/example/finalproject/MainActivity.java
+++ FinalProject/src/com/example/finalproject/MainActivity.java
@@ -1,28 +1,30 @@
 package com.example.finalproject;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
-	  private GoogleMap map;
+	private Button bStart;
 
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_main);
-	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	        .getMap();
-	    
-	    
-
-	  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+        
+        bStart = (Button) findViewById(R.id.start);
+        
+        bStart.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(this, demo.activityClass));
+			}
+		});
+        
+    }
 }
