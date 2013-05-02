e1b88e6b0d27e8f0231392a858f6495a944ebbbe
Derek Phanekham <dphanekham@smu.edu>
2013-05-02 00:47:37 -0500

    added second activity

--- FinalProject/src/com/example/finalproject/MainActivity.java
+++ FinalProject/src/com/example/finalproject/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(this, demo.activityClass));
+				startActivity(new Intent(v.getContext(), mapActivity.class));
 			}
 		});
         
---
+++ FinalProject/src/com/example/finalproject/mapActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mapActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.map_activity);
+        
+	}
+
+}
